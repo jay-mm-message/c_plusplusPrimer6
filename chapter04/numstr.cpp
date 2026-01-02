d6129e10202bf7604e8c36da51592e46ac0b3158
jay-mm-message <jay.mm.message@gmail.com>
2026-01-02 16:16:36 +0800

    update numstr.cpp

--- chapter04/numstr.cpp
+++ chapter04/numstr.cpp
@@ -5,8 +5,9 @@ int main()
     using namespace std;
     cout << "What year was your house built?\n";
     int year;
-    cin >> year;
+    //cin >> year;
     // cin.get();
+	(cin >> year).get();
     cout << "What is its street address?\n";
     char address[80];
     cin.getline(address, 80);
