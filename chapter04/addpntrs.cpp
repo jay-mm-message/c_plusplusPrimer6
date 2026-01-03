b817e048c081c70bc76deb0169bf5811b82d82df
jay-mm-message <jay.mm.message@gmail.com>
2026-01-03 14:09:46 +0800

    update addpntrs.cpp

--- chapter04/addpntrs.cpp
+++ chapter04/addpntrs.cpp
@@ -32,6 +32,33 @@ void p_stacks(short stacks[], const int sz) {
 	return;
 }
 
+// Here are two ways to get the address of an array
+void p_wages_2(double wages[], const int sz) {
+	double * pw = wages;     // name of an array = address
+	// with array element
+	cout << "pw = " << pw << ", *pw = " << *pw << endl;
+	pw = pw + 1;
+	cout << "add 1 to the pw pointer:\n";
+	cout << "pw = " << pw << ", *pw = " << *pw << "\n\n";
+}
+
+void p_stacks_2(short stacks[], const int sz) {
+	short * ps = &stacks[0]; // or use address operator
+	cout << "test case: " << endl;
+	cout << "ps = " << ps << ", *ps = " << *ps << endl;
+	ps = ps + 1;
+	cout << "add 1 to the ps pointer:\n";
+	cout << "ps = " << ps << ", *ps = " << *ps << "\n\n";
+	
+	cout << "access two elements with array notation\n";
+	cout << "stacks[0] = " << stacks[0] 
+		<< ", stacks[1] = " << stacks[1] << endl;
+	cout << "access two elements with pointer notation\n";
+
+	cout << "*stacks = " << *stacks
+		<< ", *(stacks + 1) =  " << *(stacks + 1) << endl;
+}
+
 int main()
 {
 	system("clear");
@@ -41,35 +68,22 @@ int main()
     double wages[narr_sz] = {10000.0, 20000.0, 30000.0};
     short stacks[narr_sz] = {3, 2, 1};
 
+	double *pw = wages;
+	short *ps = &stacks[0];
+
+	cout << "wages address: \t" << &wages << endl; 
+	cout << "&wages[0]: \t" << &wages[0] << endl;
+
+	cout << sizeof(wages) << " = size of wages (double[3]) array\n";
+	cout << sizeof(pw) << " = size of pw pointer\n";
+	cout << sizeof(stacks) << " = size of stacks (short[3]) array\n";
+	cout << sizeof(ps) << " = size of ps pointer\n";
 	p_wages(wages, narr_sz);
 	p_stacks(stacks, narr_sz);
-// Here are two ways to get the address of an array
-    double * pw = wages;     // name of an array = address
-    short * ps = &stacks[0]; // or use address operator
-// with array element
-    cout << "pw = " << pw << ", *pw = " << *pw << endl;
-    pw = pw + 1;
-    cout << "add 1 to the pw pointer:\n";
-    cout << "pw = " << pw << ", *pw = " << *pw << "\n\n";
 
-    cout << "ps = " << ps << ", *ps = " << *ps << endl;
-    ps = ps + 1;
-    cout << "add 1 to the ps pointer:\n";
-    cout << "ps = " << ps << ", *ps = " << *ps << "\n\n";
-
-    cout << "access two elements with array notation\n";
-    cout << "stacks[0] = " << stacks[0] 
-         << ", stacks[1] = " << stacks[1] << endl;
-    cout << "access two elements with pointer notation\n";
-
-    cout << "*stacks = " << *stacks
-         << ", *(stacks + 1) =  " << *(stacks + 1) << endl;
-
-    cout << sizeof(wages) << " = size of wages (double[3]) array\n";
-    cout << sizeof(pw) << " = size of pw pointer\n";
-
-	cout << sizeof(stacks) << " = size of stacks (short[3]) array\n";
-    cout << sizeof(ps) << " = size of ps pointer\n";
+	p_wages_2(wages, narr_sz);
+	p_stacks_2(stacks, narr_sz);
+ 
     // cin.get();
 
 	
