46df7c357339a514b02dde18c72b7212e5f1d0a3
jay-mm-message <jay.mm.message@gmail.com>
2026-01-03 13:39:45 +0800

    update addpntrs.cpp

--- chapter04/addpntrs.cpp
+++ chapter04/addpntrs.cpp
@@ -1,11 +1,34 @@
 // addpntrs.cpp -- pointer addition
 #include <iostream>
+using namespace std;
+
+void p_wages(double wages[], const int sz) {
+	for(auto *p = wages ; p != &wages[sz] ; ++p) {
+		cout << *p << ' ';
+	}
+	cout << endl;
+	return;
+}
+
+void p_stacks(short stacks[], const int sz) {
+	for(auto *p = stacks ; p != &stacks[sz] ; ++p) {
+		cout << *p << ' ';
+	}
+	cout << endl;
+	return;
+}
+
 int main()
 {
+	system("clear");
     using namespace std;
-    double wages[3] = {10000.0, 20000.0, 30000.0};
-    short stacks[3] = {3, 2, 1};
 
+	const int narr_sz = 3;
+    double wages[narr_sz] = {10000.0, 20000.0, 30000.0};
+    short stacks[narr_sz] = {3, 2, 1};
+
+	p_wages(wages, narr_sz);
+	p_stacks(stacks, narr_sz);
 // Here are two ways to get the address of an array
     double * pw = wages;     // name of an array = address
     short * ps = &stacks[0]; // or use address operator
@@ -28,8 +51,13 @@ int main()
     cout << "*stacks = " << *stacks
          << ", *(stacks + 1) =  " << *(stacks + 1) << endl;
 
-    cout << sizeof(wages) << " = size of wages array\n";
+    cout << sizeof(wages) << " = size of wages (double[3]) array\n";
     cout << sizeof(pw) << " = size of pw pointer\n";
+
+	cout << sizeof(stacks) << " = size of stacks (short[3]) array\n";
+    cout << sizeof(ps) << " = size of ps pointer\n";
     // cin.get();
+
+	
     return 0; 
 }
