3075766b6fe943aa66b9562aa931a0b44580eff8
jay-mm-message <jay.mm.message@gmail.com>
2026-01-03 09:52:48 +0800

    update init_ptr.cpp

--- chapter04/init_ptr.cpp
+++ chapter04/init_ptr.cpp
@@ -1,15 +1,34 @@
 // init_ptr.cpp -- initialize a pointer
 #include <iostream>
+
+struct student {
+	int id;
+	char name[20];
+};
+
 int main()
 {
+	system("clear");
+
     using namespace std;
     int higgens = 5;
     int * pt = &higgens;
+
+	int *n_pt;
+	double *d_pt;
+	char *c_pt;
+	student *st_pt;
+
+	cout << "sizeof(int*): " << sizeof(n_pt) << endl;
+	cout << "sizeof(double*): " << sizeof(d_pt) << endl;
+	cout << "sizeof(char*): " << sizeof(c_pt) << endl;
+	cout << "sizeof(struct student*): " << sizeof(st_pt) << endl;
 
     cout << "Value of higgens = " << higgens
          << "; Address of higgens = " << &higgens << endl;
     cout << "Value of *pt = " << *pt
          << "; Value of pt = " << pt << endl;
     // cin.get();
+	cout << endl;
     return 0; 
 }
