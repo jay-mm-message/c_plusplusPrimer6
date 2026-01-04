fac9843388538b4ede0992c08f1891fb77ae1571
jay-mm-message <jay.mm.message@gmail.com>
2026-01-04 09:42:24 +0800

    update forloop.cpp

---
+++ chapter05/bySelf/forloop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	system("clear");
+
+	const int times = 5;
+	for(auto i = 0 ; i < times ; ++i) {
+		cout << "C++ knows loops.\n";
+	}
+	cout << "C++ knows when to stop.\n";
+	cout << endl;
+	return 0;
+}
