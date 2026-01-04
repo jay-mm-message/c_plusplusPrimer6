4950c6c9d151803290e800098d5d2d2e0e598e05
jay-mm-message <jay.mm.message@gmail.com>
2026-01-04 14:27:17 +0800

    update waiting.cpp

--- chapter05/waiting.cpp
+++ chapter05/waiting.cpp
@@ -3,10 +3,14 @@
 #include <ctime> // describes clock() function, clock_t type
 int main()
 {
+	system("clear");
     using namespace std;
     cout << "Enter the delay time, in seconds: ";
-    float secs;
-    cin >> secs;
+    float secs = 5.0;
+    //cin >> secs;
+	cout << "secs: " << secs << endl;
+	cout << "CLOCKS_PER_SEC: " << CLOCKS_PER_SEC << endl;
+
     clock_t delay = secs * CLOCKS_PER_SEC;  // convert to clock ticks
     cout << "starting\a\n";
     clock_t start = clock();
