50ce1eb8ba4036e679cc382bb7bada2776392fd8
jay-mm-message <jay.mm.message@gmail.com>
2026-01-06 07:41:34 +0800

    update chapter06/bySelf/outfile.cpp

---
+++ chapter06/bySelf/outfile.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+#include <fstream>
+
+int main() {
+	system("clear");
+	ofstream outfile;
+	outfile.open("test_cast.txt");
+	outfile << "Hello Wold\n";
+	outfile << "123\n";
+	outfile << endl;
+
+	outfile.close();	
+	
+	cout << endl << endl;
+	return 0;
+}
