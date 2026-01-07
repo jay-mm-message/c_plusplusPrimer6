142017be151e28a245c04c464ae354f69c820184
jay-mm-message <jay.mm.message@gmail.com>
2026-01-07 15:01:52 +0800

    update int_fun.cpp

--- chapter07/bySelf/int_fun.cpp
+++ chapter07/bySelf/int_fun.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int sum(int a, int b) {
-	return a + b;
-}
+int sum(int, int);
 
 int main() {
 	system("clear");
 	cout << "1 + 2: " << sum(1, 2) << endl;
 	return 0;
+}
+
+int sum(int a, int b) {
+	return a + b;
 }
