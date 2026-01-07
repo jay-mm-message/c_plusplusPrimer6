b2303561679021c37e46ac64cdd01feb6a008603
jay-mm-message <jay.mm.message@gmail.com>
2026-01-07 18:32:42 +0800

    update limit.cpp

--- chapter07/bySelf/limit.cpp
+++ chapter07/bySelf/limit.cpp
@@ -5,6 +5,7 @@ int fill_array(int [], int);
 void create_array(int [], int);
 void p_array(const int [], int);
 void revalue(int , int [], int);
+void p_array_2(const int *, const int *);
 
 int main() {
 	system("clear");
@@ -14,10 +15,22 @@ int main() {
 	cout << "Max: " << fill_array(numbers, ArSize) << endl;
 	p_array(numbers, ArSize);
 	revalue(10, numbers, ArSize);
-	p_array(numbers, ArSize);
+	p_array_2(numbers + 0, numbers + ArSize);
+	//p_array(numbers, ArSize);
+
 
 	cout << endl;
 	return 0;
+}
+
+void p_array_2(const int *begin, const int *end) {
+	cout << __FUNCTION__ << endl;
+	const int *p = nullptr;
+
+	for(p = begin ; p != end ; ++p) {
+		cout << *p << ' ';
+	}
+	cout << endl;
 }
 
 void revalue(int r, int arr[], int n) {
