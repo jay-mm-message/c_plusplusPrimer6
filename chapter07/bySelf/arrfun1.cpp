2237ea5c81742e9cf3155694b411724865a1173c
jay-mm-message <jay.mm.message@gmail.com>
2026-01-07 17:52:57 +0800

    update arrfun1.cpp

---
+++ chapter07/bySelf/arrfun1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int sum(int [], int);
+void create_cookies(int [], int);
+void p_cookies(int [], int);
+
+int main() {
+	system("clear");
+	const int ArSize = 8;
+	int cookies[ArSize] {0};
+	create_cookies(cookies, ArSize);
+	p_cookies(cookies, ArSize);
+
+	int sum_result = sum(cookies, ArSize);
+
+	cout << "Total cookies eaten: " << sum_result << endl;
+
+	cout << endl;
+	return 0;
+}
+
+void p_cookies(int arr[], int n) {
+	for(int i = 0 ; i < n ; ++i) {
+		cout << arr[i] << ' ';
+	}
+	cout << endl;
+}
+
+void create_cookies(int arr[], int n) {
+	for(int i = 0, j = 1 ; i < n ; ++i, j *= 2) {
+		arr[i] = j;
+	}
+}
+
+int sum(int arr[], int n) {
+	int result {0};
+	for(int i = 0 ; i < n ; ++i) {
+		result = result + arr[i];
+	}
+	return result;
+}
